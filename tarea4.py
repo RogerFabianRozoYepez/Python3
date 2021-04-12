17dd92c69aa5da5dd84eb9ffef8bc7368638d3f0
RogerFabianRozoYepez <73122923+RogerFabianRozoYepez@users.noreply.github.com>
2021-04-12 18:46:13 -0500

    Add files via upload

---
+++ tarea4.py
@@ -0,0 +1,6 @@
+n=int(input("Ingrese N: "))
+a=[]
+for i in range (n):
+    a.append(int(input("ingrese su/sus numeros")))
+a.sort()
+print("Lista ordenada: ",a)
---
+++ tarea5.py
@@ -0,0 +1,3 @@
+b = 2
+dec = int(str(b),2)
+print("El decimal es: ",dec)
